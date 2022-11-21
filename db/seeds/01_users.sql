45f98f8eb0121e0b7869f964dca2532664b4ebb7
RuoningJin <ronjin2013@hotmail.com>
2022-11-21 14:35:03 -0500

    all password set to password for test purposes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, password) VALUES ('alice123', 'asdfljksjdf');
-INSERT INTO users (username, password) VALUES ('kira123', 'asdsdfjkljsdf');
-INSERT INTO users (username, password) VALUES ('lila123', 'asdsdfjkljsdf');
-INSERT INTO users (username, password) VALUES ('ron123', 'asdsdfjkljsdf');
-INSERT INTO users (username, password) VALUES ('stefan123', 'asdsdfjkljsdf');
-INSERT INTO users (username, password) VALUES ('kelvin123', 'asdsdfjkljsdf');
-INSERT INTO users (username, password) VALUES ('hello123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('alice123', 'password');
+INSERT INTO users (username, password) VALUES ('kira123', 'password');
+INSERT INTO users (username, password) VALUES ('lila123', 'password');
+INSERT INTO users (username, password) VALUES ('ron123', 'password');
+INSERT INTO users (username, password) VALUES ('stefan123', 'password');
+INSERT INTO users (username, password) VALUES ('kelvin123', 'password');
+INSERT INTO users (username, password) VALUES ('hello123', 'password');
