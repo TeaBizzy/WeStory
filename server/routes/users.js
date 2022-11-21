3870a1c6124a769f0900aeb6f50370f14d9efc9a
Stefan Talbot <satalbot@protonmail.com>
2022-11-21 08:43:51 -0500

    Modify Route
    ~ Modified: Users route.
    + Added: Structure comments to users.js.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,29 @@
+// ___________________________________________________________________________ //
+// *----------------------------- Documentation -----------------------------* //
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
+* All routes for Users are defined here
+* Since this file is loaded in server.js into /users,
+* these routes are mounted onto /users
+* See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+*/
+
+
+// ___________________________________________________________________________ //
+// *----------------------------- Configuration -----------------------------* //
 
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+
+// ___________________________________________________________________________ //
+// *-------------------------------- Routing --------------------------------* //
+
+router.get('/:id', (req, res) => {
+  res.send(`User: ${req.params.id} Stories`)
+});
+
+
+// ___________________________________________________________________________ //
+// *-------------------------------- Exports --------------------------------* //
 
 module.exports = router;
