4d5ef2ef5bf4f8d3ffe03861a2dc043a4fe076ba
RuoningJin <ronjin2013@hotmail.com>
2022-11-21 11:18:59 -0500

    users and stories table added

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, password) VALUES ('alice123', 'asdfljksjdf');
+INSERT INTO users (username, password) VALUES ('kira123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('lila123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('ron123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('stefan123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('kelvin123', 'asdsdfjkljsdf');
+INSERT INTO users (username, password) VALUES ('hello123', 'asdsdfjkljsdf');
