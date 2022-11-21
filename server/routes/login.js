f1f01f66002eea5513b822362abf74abd91d05fd
Stefan Talbot <satalbot@protonmail.com>
2022-11-21 07:32:02 -0500

    Add Comments
    + Added: Structure comments.

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,12 +1,22 @@
+// ___________________________________________________________________________ //
+// *----------------------------- Documentation -----------------------------* //
 /*
- * All routes for Login are defined here
- * Since this file is loaded in server.js into /login,
- *   these routes are mounted onto /login
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
+* All routes for Login are defined here
+* Since this file is loaded in server.js into /login,
+*   these routes are mounted onto /login
+* See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+*/
+
+
+// ___________________________________________________________________________ //
+// *----------------------------- Configuration -----------------------------* //
 
 const express = require('express');
 const router  = express.Router();
+
+
+// ___________________________________________________________________________ //
+// *-------------------------------- Routing --------------------------------* //
 
 router.get('/', (req, res) => {
   res.send('Login Page');
@@ -15,5 +25,9 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   res.send('Logged In!')
 });
+
+
+// ___________________________________________________________________________ //
+// *-------------------------------- Exports --------------------------------* //
 
 module.exports = router;
