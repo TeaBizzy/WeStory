e3043045bb186f83d60f62385e3c53c581bf2881
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-11-22 13:59:44 -0500

    Add functionality to back-to-top button and create button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,18 @@
 // Client facing scripts here
-$(document).ready(function() {
-    loadStory();
-  });
+$(document).ready(function () {
+  loadStory();
+});
 
 // Renders each story to the page
-const renderStories = function(stories) {
+const renderStories = function (stories) {
   for (const story of stories) {
     const $story = addStory(story);
-    $('#story-container').prepend($story);
+    $("#story-container").prepend($story);
   }
 };
 
 // Populates story html template using the given story data. Returns the finished html
-const addStory = function(storyData) {
+const addStory = function (storyData) {
   console.log(storyData);
   const markup = `
   <a href="/stories/${storyData.id}">
@@ -33,9 +33,8 @@ const addStory = function(storyData) {
 };
 
 // Fetches story data with an async request from the API
-const loadStory = function() {
-  $.get('/api/stories/')
-    .then(function(storyData) {
-      renderStories(storyData.stories);
-    });
+const loadStory = function () {
+  $.get("/api/stories/").then(function (storyData) {
+    renderStories(storyData.stories);
+  });
 };
