47262a91977492afb207195f18385e654cc8eee7
Stefan Talbot <satalbot@protonmail.com>
2022-11-22 10:41:10 -0500

    Add Links
    + Added: Links to story cards, that redirect to that specific story page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,16 +14,16 @@ const renderStories = function(stories) {
 // Populates story html template using the given story data. Returns the finished html
 const addStory = function(storyData) {
   const markup = `
-    <article class="story">
-    <div class="story-header">
-      <span class="story-title">${storyData.title}</span>
-      <span class="story-creator-handle">@creator-username</span>
-    </div>
-    <div class="story-content">
-      <i class="fa-solid fa-book"></i>
-      <p class="story-paragraph">${storyData.content}</p>
-    </div>
-    </article>`;
+  <a href="/stories/${storyData.id}"><article class="story">
+  <div class="story-header">
+  <span class="story-title">${storyData.title}</span>
+  <span class="story-creator-handle">@creator-username</span>
+  </div>
+  <div class="story-content">
+  <i class="fa-solid fa-book"></i>
+  <p class="story-paragraph">${storyData.content}</p>
+  </div>
+  </article></a>`;
   return markup;
 };
 
