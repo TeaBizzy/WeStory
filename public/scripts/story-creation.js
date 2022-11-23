9a53dc47f85b47ff26b4136f3ae16639481b0c6c
Stefan Talbot <satalbot@protonmail.com>
2022-11-22 21:28:34 -0500

    Implement Story Creation
    + Added: Ability for user to create a new story.
    + Added: Redirect to GET /stories/:story_id using the newly create story id upon creation.

--- public/scripts/story-creation.js
+++ public/scripts/story-creation.js
@@ -3,18 +3,30 @@ const submitStory = function(event) {
 
   const textArea = $('#story-text');
   const storyForm = textArea.parent();
-  const storyContent = textArea.val();
-  const contentLength = storyContent.length;
+  const content = textArea.val();
+  const contentLength = content.length;
+  const user_id = $('body').attr('data-userId');
   console.log(contentLength)
 
   if(contentLength > 160) {
-    console.log(`Error: Submission length is: ${storyContent.length} must be under 160 characters!`);
+    console.log(`Error: Submission length is: ${contentLength} must be under 160 characters!`);
   }
 
   if(contentLength <= 0) {
     console.log(`Error: Submission can't be empty!`);
   }
 
-  console.log(storyContent);
   storyForm.trigger("reset");
+  const newStory = {
+    user_id,
+    content,
+    title: `Story ${Math.floor(Math.random() * 1000)}`
+  }
+
+  $.post('/api/stories', newStory)
+  .then(data => {
+    const storyId = data.story.id;
+    window.location.href=`/stories/${storyId}`;
+  });
+
 };
