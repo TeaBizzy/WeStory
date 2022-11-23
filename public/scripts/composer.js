16b3c97d11918b8f8dec45fba39f390c1c7dcf10
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-11-22 21:04:28 -0500

    Fix additional <a> tag css changes

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,29 +1,31 @@
-// Click nav bar button to hide/show new story form
-$(".new-story").hide();
+$(document).ready(function () {
+  // Click nav bar button to hide/show new story form
+  $(".new-story").hide();
 
-$(".create-button").on("click", () => {
-  if ($(".new-story").first().is(":hidden")) {
-    $(".new-story").slideDown(800);
-    $(".welcome").css("margin-top", 30);
-  } else {
-    $(".new-story").slideUp(800);
-    $(".welcome").css("margin-top", 150);
-  }
-});
+  $(".create-button").on("click", () => {
+    if ($(".new-story").first().is(":hidden")) {
+      $(".new-story").slideDown(800);
+      $(".welcome").css("margin-top", 30);
+    } else {
+      $(".new-story").slideUp(800);
+      $(".welcome").css("margin-top", 150);
+    }
+  });
 
-$("footer").hide();
+  // Function to show button if user scrolls 100 pixels
+  $("footer").hide();
 
-// Function to show button if user scrolls 100 pixels
-$(window).on("scroll", function () {
-  if ($(this).scrollTop() > 100) {
-    $("footer").fadeIn();
-  } else {
-    $("footer").fadeOut();
-  }
-});
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("footer").fadeIn();
+    } else {
+      $("footer").fadeOut();
+    }
+  });
 
-// function to take user back to the top after clicking the button
-$(".fa-arrow-up").on("click", function () {
-  $("html, body").animate({ scrollTop: 0 }, 800);
-  return false;
+  // function to take user back to the top after clicking the button
+  $(".fa-arrow-up").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 });
