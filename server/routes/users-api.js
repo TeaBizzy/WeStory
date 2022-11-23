3ef5608e7fe8771192fe0fce556d67a5ba1a0046
RuoningJin <ronjin2013@hotmail.com>
2022-11-22 23:03:15 -0500

    changed route

--- server/routes/users-api.js
+++ server/routes/users-api.js
@@ -5,29 +5,29 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-const stories = require('../../db/queries/stories');
-// const userQueries = require('../../db/queries/users');
+// const express = require('express');
+// const router  = express.Router();
+// const stories = require('../../db/queries/stories');
+// // const userQueries = require('../../db/queries/users');
 
-// router.get('/', (req, res) => {
-//   // userQueries.getUsers()
-//   //   .then(users => {
-//   //     res.json({ users });
-//   //   })
-//   //   .catch(err => {
-//   //     res
-//   //       .status(500)
-//   //       .json({ error: err.message });
-//   //   });
+// // router.get('/', (req, res) => {
+// //   // userQueries.getUsers()
+// //   //   .then(users => {
+// //   //     res.json({ users });
+// //   //   })
+// //   //   .catch(err => {
+// //   //     res
+// //   //       .status(500)
+// //   //       .json({ error: err.message });
+// //   //   });
+// // });
+
+// router.get('/:id', (req, res) => {
+
 // });
 
-router.get('/:id', (req, res) => {
+// router.post('/', (req, res) => {
+//   res.send('Insert a new user to the users table');
+// });
 
-});
-
-router.post('/', (req, res) => {
-  res.send('Insert a new user to the users table');
-});
-
-module.exports = router;
+// module.exports = router;
