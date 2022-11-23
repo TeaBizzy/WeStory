dc590f6396d679488f85ddf344d1376117c59f6c
Stefan Talbot <satalbot@protonmail.com>
2022-11-22 22:42:17 -0500

    Add Authentication To API Routes
    + Added: Authentication to all /api/stories/ routes.

--- server/routes/stories-api.js
+++ server/routes/stories-api.js
@@ -19,6 +19,16 @@ const stories = require('../../db/queries/stories');
 
 // Returns a JSON object containing an array of story objects
 router.get('/', (req, res) => {
+  // Get session cookie
+  const sessionCookie = req.session.user_id;
+  const isLoggedIn = sessionCookie ? true : false;
+
+  // Validates requester is authorized
+  if (!isLoggedIn) {
+    err = new Error('Access Denied')
+    return res.status(401).send();
+  }
+
   stories.getStories()
     .then(stories => {
       res.json({ stories });
@@ -32,7 +42,18 @@ router.get('/', (req, res) => {
 
 // the parameters passed would be inputed by users and needs to be changed
 router.post('/', (req, res) => {
+  // Get session cookie
+  const sessionCookie = req.session.user_id;
+  const isLoggedIn = sessionCookie ? true : false;
+
+  // Validates requester is authorized
+  if (!isLoggedIn) {
+    err = new Error('Access Denied')
+    return res.status(401).send();
+  }
+
   const newStory = req.body;
+
   stories.addStory(newStory)
     .then(story => {
       res.json({story});
@@ -46,6 +67,16 @@ router.post('/', (req, res) => {
 
 // the id would be passed in when clicked on the container (the {id: 1} neeeds to be changed)
 router.get('/:id', (req, res) => {
+  // Get session cookie
+  const sessionCookie = req.session.user_id;
+  const isLoggedIn = sessionCookie ? true : false;
+
+  // Validates requester is authorized
+  if (!isLoggedIn) {
+    err = new Error('Access Denied')
+    return res.status(401).send();
+  }
+
   stories.getStoryById({id: 5})
     .then(story => {
       res.json({ story });
@@ -58,10 +89,30 @@ router.get('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  // Get session cookie
+  const sessionCookie = req.session.user_id;
+  const isLoggedIn = sessionCookie ? true : false;
+
+  // Validates requester is authorized
+  if (!isLoggedIn) {
+    err = new Error('Access Denied')
+    return res.status(401).send();
+  }
+
   res.send(`Modify story: ${req.params.id} & display it`);
 });
 
 router.get('/user/:id', (req, res) => {
+  // Get session cookie
+  const sessionCookie = req.session.user_id;
+  const isLoggedIn = sessionCookie ? true : false;
+
+  // Validates requester is authorized
+  if (!isLoggedIn) {
+    err = new Error('Access Denied')
+    return res.status(401).send();
+  }
+
   stories.getStoryByUserId({id: 1})
     .then(story => {
       res.json({ story });
