147ae7b261019d5eada14942c5429ff20565fa7e
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-11-23 03:19:17 -0500

    Finish storyid page functionality

---
+++ public/scripts/story-page.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $("footer").hide();
+
+  $(window).on("scroll", function () {
+    let togglePosition = $(".toggle-contributions").offset().top;
+    let toggleHeight = $(".toggle-contributions").outerHeight();
+    let windowHeight = $(window).height();
+    if ($(window).scrollTop() > togglePosition + toggleHeight - windowHeight) {
+      $("footer").fadeIn();
+    } else {
+      $("footer").fadeOut();
+    }
+  });
+
+  // function to take user back to the top after clicking the button
+  $(".fa-arrow-up").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+  $(".toggle-contributions").on("click", () => {
+    $("html, body").animate({ scrollTop: $(".story").height() }, 800);
+  });
+});
